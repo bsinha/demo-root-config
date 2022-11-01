2a3f496269ed87e03d0e3aeafb35f7c3b72cbe79
Pooja4183 <poojatomar.jt@gmail.com>
2022-11-01 07:08:04 +0530

    cosmetic changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 9000;
 
 express()
   .use(cors())
