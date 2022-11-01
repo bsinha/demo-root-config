f38c87f98a4859aef31bc36b2a5baf3a31b7407d
Pooja4183 <poojatomar.jt@gmail.com>
2022-11-01 06:49:07 +0530

    added cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 express()
-  .use(express.static(path.join(__dirname, "dist")))
-  .get("*", (req, res) => {
-    res.sendFile("index.html", { root: "dist" });
+  .use(cors())
+  .use(express.static(path.join(__dirname, 'dist')))
+  .get('*', (req, res) => {
+    res.sendFile('index.html', { root: 'dist' });
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
