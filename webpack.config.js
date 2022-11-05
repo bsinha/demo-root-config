19d08a873be3e94f5c0af82fbc2b28fcb51e30bc
Bipul Sinha <bipulsinha@yahoo.com>
2022-11-05 08:21:13 +0530

    back to 1st draft

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const path = require("path");
 
 module.exports = (webpackConfigEnv, argv) => {
   const orgName = "bipsin";
@@ -16,48 +14,13 @@ module.exports = (webpackConfigEnv, argv) => {
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
-    output: {
-      publicPath:
-        argv.mode === "development"
-          ? "http://localhost:8080/"
-          : "https://demo-root-config.netlify.app/",
-    },
-  
-    resolve: {
-      extensions: [".jsx", ".js", ".json"],
-    },
-  
-    devServer: {
-      port: 8080,
-      historyApiFallback: true,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-      }
-    },
-    // entry: {
-    //   polyfills: path.resolve(__dirname, "src") + "/polyfills.ts",
-    //   index: path.resolve(__dirname, "src") + "/root-config.ts",
-    //   "root-page": path.resolve(__dirname, "src") + "/apps/root-page.ts",
-    // },
-    // output: {
-    //   path: path.resolve(__dirname, "./dist"),
-    //   filename: "[name].js",
-    // },
     plugins: [
-      new ModuleFederationPlugin({
-        name: "@bipsin/root-config",
-        filename: "remoteEntry.js",
-        remotes: {},
-        exposes: {
-        //  "./Header": "./src/Header",
-        },
-        shared: require("./package.json").dependencies,
-      }),
       new HtmlWebpackPlugin({
         inject: false,
         template: "src/index.ejs",
         templateParameters: {
           isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
+          orgName,
         },
       }),
     ],
